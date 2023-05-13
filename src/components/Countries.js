ceb4d2f7b73f588c3e4c1b6bfb6a5cf8fa7565a0
Vict0r <viton4ik@gmail.com>
2023-05-13 23:14:17 +0200

    saving

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -6,7 +6,7 @@ import Country from "./Country";
 import "../styles/Countries.css";
 
 
-//https://restcountries.com/v3.1/all
+//https://restcountries.com/v3.1/all - API
 
 function Countries() {
     const url = "https://restcountries.com/v3.1/all";
@@ -15,7 +15,7 @@ function Countries() {
     if (!countries.length) {
         axios.get(url).then(res => {
             console.log('res', res);
-            // console.log('res1', res.data[0]);
+            // console.log('res1', res.altSpellings);
             setCountries(res.data);
         });
 
@@ -23,9 +23,22 @@ function Countries() {
 
     return(
         <Table striped bordered hover className={"countries"}>
-            <thead><tr><th>Number</th><th>Country</th><th>Capital</th><th>Activity</th></tr></thead>
+            <thead><tr><th>Number</th><th>Country</th><th>Label</th><th>Capital</th><th>Activity</th></tr></thead>
             <tbody>
-                {countries.map(country => <Country key={country.altSpellings[0]} country={country}/>)}
+            {/* 1 option */}
+            {/*    {countries.map(country => <Country key={country.altSpellings[0]} country={country}/>)}*/}
+            {/* 2 option */}
+            {/*    {countries.map(country => country.capital ? <Country key={country.altSpellings[0]}*/}
+                {countries.map((country, num) => country.capital ? <Country key={country.altSpellings[0]}
+                     map={country.maps.openStreetMaps}
+                     num={num}
+                     name={country.name.common}
+                     label={country.altSpellings}
+                     capital={country.capital}/> : <Country key={country.altSpellings[0]}
+                                                            map={country.maps.openStreetMaps}
+                                                            num={num}
+                                                            name={country.name.common}
+                                                            label={country.altSpellings}/>)}
             </tbody>
         </Table>
     );
--- src/components/Country.js
+++ src/components/Country.js
@@ -1,25 +1,52 @@
 import React, {useState}  from "react";
 import Button from "react-bootstrap/Button";
+import PropTypes from "prop-types";
 
 import "../styles/Country.css";
-
+let number = 1;
 function Country(props) {
-    let number = 0;
+    // let number = 0;
     const [selected, changeSelected] = useState(false);
+
 
     return(
         <tr className={ selected ? "selected-country": ""}>
-            <td>{ number++ }</td>
-            <td>{ props.country.altSpellings[0] }, { props.country.altSpellings[1] }, { props.country.altSpellings[2] }</td>
-            <td>{ props.country.capital }</td>
+            {/*<td>{ number++ }</td>*/}
+            <td>{ props.num + 1 }</td>
+        {/* old option */}
+            {/*<td>{ props.country.altSpellings[1] }, { props.country.altSpellings[2] } ({ props.country.altSpellings[0] })</td>*/}
+            {/*    <td>{ props.country.altSpellings.slice(1).join('/') }</td>*/}
+        {/* 1 option */}
+        {/*    <td>{ props.country.name.common }</td>*/}
+        {/*    <td>{ props.country.altSpellings[0] }</td>*/}
+        {/*    <td>{ props.country.capital }</td>*/}
+        {/* 2 option */}
+            <td>{ props.name }</td>
+            <td>{ props.label[0] }</td>
+            <td>{ props.capital }</td>
             <td>
                 { !selected ?
-                    <Button variant="success" onClick={() => changeSelected(true)}>Select</Button>:
+                    <a href={ props.map } target="_blank">
+                    <Button variant="success" onClick={() => changeSelected(true)}>Select</Button>
+                    </a>:
                     <Button variant="danger" onClick={() => changeSelected(false)}>Cancel</Button>
                 }
             </td>
         </tr>
     );
 }
+
+Country.defaultProps = {
+    capital: "Not Avaliable",
+    name: "N/A"
+}
+
+// data validation. Development mode ONLY!
+// Country.propTypes = {
+//     country: PropTypes.oneOfType([
+//         PropTypes.object,
+//         PropTypes.array
+//     ])
+// };
 
 export default Country;
